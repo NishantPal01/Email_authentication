f424eca7c7db8b5a17997c2fc04bc452ddbdab7a
nishant pal <nishantpal000@gmail.com>
2022-06-26 17:56:16 +0530

    email_authenticate_app

---
+++ src/urls.py
@@ -0,0 +1,14 @@
+from django import views
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login', views.login_attempt, name="login_attempt"),
+    path('register', views.register_attempt, name="register_attempt"),
+    path('success', views.success, name="success"),
+    path('token', views.token_send , name="token_send"),
+    path('verify/<auth_token>' , views.verify , name='verify'),
+    path('error' , views.error_page , name='error')
+]
